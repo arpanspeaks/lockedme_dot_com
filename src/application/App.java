ee88144de21a94cf13b6b8d8db2b2ded4cded741
Arpan Sardar <arpanspeaks@gmail.com>
2022-02-05 21:34:18 +0530

    basic func added

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,17 @@
 package application;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
+		
+		File dir = new File("G:\\Learning\\Simplilearn\\Assessments\\locked-me\\src\\file_dir");
+		if (!dir.exists()) dir.mkdirs();
 		
 		String choice = "";
 		Scanner sc = new Scanner(System.in);
@@ -24,19 +30,67 @@ public class App {
 			
 			switch(choice) {
 				case "1": {
-					System.out.println("1");
+					System.out.println("Files residing in the directory:");
+					System.out.println("******************************");
+					for(var f : dir.list()) {
+						System.out.println(f);
+					}
+					System.out.println();
 					break;
 				}
 				case "2": {
-					System.out.println("2");
+					System.out.print("Enter the filename with extension you want to add: ");
+					String filename = sc.nextLine();
+					File file = new File(dir + "\\" + filename);
+					if(!file.exists()) {
+						try {
+							file.createNewFile();
+							System.out.println("File created successfully...");
+						} catch(Exception e) {
+							System.out.println("Some error occured...");
+						}
+					}
+					else
+						System.out.println("File already present...");
+					System.out.println();
 					break;
 				}
 				case "3": {
-					System.out.println("3");
+					System.out.print("Enter the filename with extension you want to delete: ");
+					String filename = sc.nextLine();
+					File file = new File(dir + "\\" + filename);
+					if(file.exists()) {
+						try {
+							file.delete();
+							System.out.println("File deleted successfully...");
+						} catch(Exception e) {
+							System.out.println("Some error occured...");
+						}
+					}
+					else
+						System.out.println("File not present...");
+					System.out.println();
 					break;
 				}
 				case "4": {
-					System.out.println("4");
+					System.out.print("Enter the filename with extension you want to search: ");
+					String filename = sc.nextLine();
+					File file = new File(dir + "\\" + filename);
+					if(file.exists()) {
+						System.out.println(file.getAbsolutePath());
+						System.out.println("*********************");
+						try {
+							Files
+								.readAllLines(Paths.get(file.getAbsolutePath()))
+								.stream()
+								.forEach(System.out::println);
+						} catch (IOException e) {
+							System.out.println("An error occured...");
+						}
+					}
+					else
+						System.out.println("File not present...");
+					System.out.println();
 					break;
 				}
 				case "5": {
